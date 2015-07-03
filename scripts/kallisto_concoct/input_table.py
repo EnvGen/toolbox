d624220fbfcc02d4ca061589124d0f5aafe42ed6
Johannes Alneberg <alneberg@kth.se>
2015-07-03 10:05:12 +0200

    Input table for kallisto should now work with sample names

--- scripts/kallisto_concoct/input_table.py
+++ scripts/kallisto_concoct/input_table.py
@@ -6,11 +6,20 @@ import pandas as pd
 import os
 import sys
 
+def samplenames_from_file(name_file):
+    if name_file:
+        with open(name_file, 'r') as name_file_h:
+            return [l.strip() for l in name_file_h]
+    else:
+        return None
+
 def main(args):
     sample_dfs = []
+
+    samplenames = samplenames_from_file(args.samplenames)
     for i, sample in enumerate(args.quantfiles):
-        if args.samplenames:
-            samplename = args.samplenames[i]
+        if samplenames:
+            samplename = samplenames[i]
         else:
             samplename = os.path.basename(sample)        
         sample_df = pd.read_table(sample, index_col=0)
@@ -28,4 +37,5 @@ if __name__ == "__main__":
     parser.add_argument("quantfiles", nargs='+', help="Kallisto abundance.txt files")
     parser.add_argument("--samplenames", default=None, help="File with sample names, one line each, Should be the same order and the same number as the abundance.txt files")
     args = parser.parse_args()
+    
     main(args)
