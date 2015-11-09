5dced32feacf48816ba9380d9c321ecf00a226af
Johannes Alneberg <alneberg@kth.se>
2015-11-09 14:32:59 +0100

    Filter fasta on length using streams

--- scripts/filter_fasta_on_length.py
+++ scripts/filter_fasta_on_length.py
@@ -2,7 +2,7 @@
 """filter_fasta_on_length.py 
 
 Filter a fasta file so that the resulting sequences are all longer 
-or equal to the length threshold parameter."""
+or equal to the length threshold parameter. Only accepts stdin."""
 
 import argparse
 import sys
@@ -10,22 +10,19 @@ from Bio import SeqIO
 
 def main(args):
     seqs = []
-    for i, seq in enumerate(SeqIO.parse(args.fasta_file, "fasta")):
+    for i, seq in enumerate(SeqIO.parse(sys.stdin, "fasta")):
         if len(seq) >= args.length_threshold:
             seqs.append(seq)
-    if args.output_file:
-        with open(args.output_file, 'w') as ofh:
-            SeqIO.write(seqs, ofh, 'fasta')
-    else:
+        if i % 1000 == 0 and len(seqs):
+            SeqIO.write(seqs, sys.stdout, 'fasta')
+            seqs = []
+    
+    if len(seqs):
         SeqIO.write(seqs, sys.stdout, 'fasta')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("fasta_file", help="Input Fasta file.")
     parser.add_argument("-l", "--length_threshold", type=int, help="Length trheshold to filter on.")
-    parser.add_argument('-o', '--output_file',
-        help=("Optional output file where sequences will be printed." 
-            " Otherwise use stdout."))
 
     args = parser.parse_args()
 
