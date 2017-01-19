e38e7578b3ca6084a6b7d4ca419ae0ab48587d22
Johannes Alneberg <alneberg@kth.se>
2017-01-19 10:07:31 +0100

    Fixed a bug in dbcan where the bitscores were wrong

--- scripts/hmmer_filtering/dbcan_strict_filtering.py
+++ scripts/hmmer_filtering/dbcan_strict_filtering.py
@@ -9,16 +9,33 @@ import argparse
 import sys
 
 def main(args):
-    saved_rows = {}
+    domain_score = {}
+    with open(args.domain_pos_and_score) as ifh:
+        for line in ifh.readlines():
+            line = line.strip()
+            hmm_id, protein_id, e_value, aln_start, aln_end, score = line.split('\t')
+            domain_score[(hmm_id, protein_id, e_value, aln_start, aln_end)] = score
+
     with open(args.infile) as ifh:
         for line in ifh.readlines():
+            line = line.strip()
             line_fields = line.split('\t')
+
+            hmm_id, protein_id, e_value, aln_start, aln_end = (line_fields[0], line_fields[2], line_fields[4], line_fields[5], line_fields[6])
+            
+            try:
+                score = domain_score[(hmm_id, protein_id, e_value, aln_start, aln_end)]
+            except KeyError:
+                sys.stderr.write("Tuple: {}".format((hmm_id, protein_id, e_value, aln_start, aln_end)))
+                raise
+
             if float(line_fields[4]) < float(1e-18):
                 if float(line_fields[9]) > 0.35:
-                    sys.stdout.write(line)
+                    sys.stdout.write(line + '\t' + score + '\n')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = __doc__)
     parser.add_argument("infile", help=("output from dbcan hmmscan-parser.sh script"))
+    parser.add_argument("domain_pos_and_score", help="A file with the score for each domain alignment, contains positions to assert uniqueness")
     args = parser.parse_args()
     main(args)
